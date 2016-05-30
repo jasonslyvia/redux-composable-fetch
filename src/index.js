452b719240289b121f313d60cce465a8c1f18ad3
jasonslyvia <jasonslyvia@gmail.com>
2016-05-30 11:11:25 +0800

    fix: remove unused code and adjust export

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function hasOwn(obj, ...properties) {
  *   @param  {function} onReject    Injection point when anything goes wrong, it should return a Promise
  * @return {function}
  */
-export default function createFetchMiddleware(options = {}) {
+module.exports = exports.default = function createFetchMiddleware(options = {}) {
   const { beforeFetch = defaultBeforeFetch, afterFetch = defaultAfterFetch, onReject = rejectHandler } = options;
   return () => next => action => {
     if (!action.url || !action.types) {
@@ -144,4 +144,6 @@ export default function createFetchMiddleware(options = {}) {
       }
     );
   };
-}
+};
+
+exports.applyFetchMiddleware = require('./applyFetchMiddleware');
